49f5408d6006dd3f1fe3b9b1f68f30590b01d6d6
rafael.rafades@gmail.com <rafael.rafades@gmail.com@d0083e2e-fec8-11dd-802b-177316e4ac06>
2009-04-17 01:15:42 +0000

    Arquivo para funçoes gerais em Javascript

---
+++ trunk/webroot/js/funcoesGerais.js
@@ -0,0 +1,70 @@
+/*****
+ *funções para validaçao 
+ * usando expressao regular
+ *****/
+/*
+	Nome:  ValidaEmail
+	Função: Verificar se email está no formato correto
+	parametro: mailValue
+	retorno: true ou false
+ */
+function ValidaEmail(mailValue)
+{
+	//declaraçao da expressao regular
+	var email = /^([a-z0-9_\.]{1,}@[a-z0-9_]{3,}\.[a-z]{2,5})(\.[a-z0-9_]{2,})?$/
+   
+	//teste
+	if (email.test(mailValue))
+	{
+		//alert("Tudo Ok ");
+		return true;
+	}
+	else
+	{
+		//alert("Erro");
+		return false;
+	}
+}
+
+/*
+	Nome:  ValidaTelFixo
+	Função: Verificar se telefone está no formato correto
+	parametro: telValue
+	retorno: true ou false
+ */
+function ValidaTelFixo(telValue)
+{
+   var tel = /^([^8|9|0][0-9]{6,7})$/;
+   
+   if (tel.test(telValue))
+   {
+      //alert("Tudo Ok ");
+      return true;
+   }
+   else
+   {
+      //alert("Erro");
+      return false;
+   }
+}
+/*
+	Nome:  ValidaTelFixo
+	Função: Verificar se telefone está no formato correto
+	parametro: telValue
+	retorno: true ou false
+ */
+function ValidaTel(telValue)
+{
+   var tel = /^([0-9]{7,8})$/;
+   
+   if (tel.test(telValue))
+   {
+      //alert("Tudo Ok ");
+      return true;
+   }
+   else
+   {
+      //alert("Erro");
+      return false;
+   }
+}
