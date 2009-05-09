14f4cf8285c5480518099dc940cbe725389dcf21
sistemas.roger <sistemas.roger@d0083e2e-fec8-11dd-802b-177316e4ac06>
2009-05-09 00:26:13 +0000

    Conteudo de campos necessários e contrução do método ajax de tipo de problema por área.

--- trunk/controllers/chamados_controller.php
+++ trunk/controllers/chamados_controller.php
@@ -2,11 +2,15 @@
 class ChamadosController extends AppController {
 
 	var $name = 'Chamados';
-	var $helpers = array('Html', 'Form');
+	//var $helpers = array('Html', 'Form'); 
+	var $uses = array('Chamado', 'Setor', 'Problema', 'ChamadoHistorico');
 
 	function index() {
-		$this->Chamado->recursive = 0;
-		$this->set('chamados', $this->paginate());
+		/*s$this->paginate = array('limit' => 5, 
+				'where' => "usuario_id = '$this->usuarioId'");
+		$this->Chamado->recursive = 1;
+		$this->set('chamados', $this->paginate());*/
+		$this->redirect('/home/meusChamados');
 	}
 
 	function view($id = null) {
@@ -14,10 +18,21 @@ class ChamadosController extends AppController {
 			$this->Session->setFlash(__('Invalid Chamado.', true));
 			$this->redirect(array('action'=>'index'));
 		}
-		$this->set('chamado', $this->Chamado->read(null, $id));
+		
+		$chamado = $this->Chamado->read(null, $id);
+		$setor = $this->Setor->read(null, $chamado['Problema']['setor_id']);
+		$historicos = $this->ChamadoHistorico->find('all', array(
+				'conditions' => array ('ChamadoHistorico.chamado_id' => $id),
+				'order' => 'ChamadoHistorico.id DESC'
+		));
+		$this->set(compact('chamado', 'setor', 'historicos'));
 	}
-
-	function add() {
+	
+	var $recursive = 1;
+	
+	function abrirChamado() {
+		//pr($this);
+		$this->pageTitle = "Abertura de Chamado";
 		if (!empty($this->data)) {
 			$this->Chamado->create();
 			if ($this->Chamado->save($this->data)) {
@@ -27,11 +42,20 @@ class ChamadosController extends AppController {
 				$this->Session->setFlash(__('The Chamado could not be saved. Please, try again.', true));
 			}
 		}
+		$this->Setor->recursive = 1;
+		
+		
+		$areas = $this->Setor->find('list', array (
+			'order' => 'Setor.descricao ASC',
+			'conditions' => array (
+				'Setor.atende_chamado' => 1)
+		));
+		
+		//$usuarios = $this->Chamado->Usuario->find('list');
 		$problemas = $this->Chamado->Problema->find('list');
-		$usuarios = $this->Chamado->Usuario->find('list');
 		$status = $this->Chamado->Status->find('list');
 		$responsaveis = $this->Chamado->Responsavel->find('list');
-		$this->set(compact('problemas', 'usuarios', 'status', 'responsaveis'));
+		$this->set(compact('problemas', 'usuarios', 'status', 'responsaveis', 'areas'));
 	}
 
 	function edit($id = null) {
@@ -67,6 +91,33 @@ class ChamadosController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 	}
-
+	
+	/*
+	 * Lista todos os chamados do usuário solicitante
+	 * 
+	 */
+	function meusChamados(){
+		$this->pageTitle = "Meus Chamados";
+		$this->paginate = array(
+			'limit' => 5, 
+			'conditions' => array (
+				'Chamado.usuario_id' => $this->usuarioId,
+			)
+		);
+		$this->Chamado->recursive = 2;
+		$this->set('chamados', $this->paginate());
+	}
+	
+	function ajaxListaProblemaPorArea($codigoArea){
+		
+		$this->layout = 'ajax';
+		$problemas = $this->Problema->find('list', array (
+			'order' => 'Problema.descricao ASC',
+			'conditions' => array (
+				'Problema.setor_id' => $codigoArea)
+		));
+		$this->set('problemas', $problemas);
+		//echo $problemas;
+	}
 }
 ?>
