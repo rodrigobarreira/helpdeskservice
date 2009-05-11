26ee17deed9dd995d27458a8e970b44cdc25bbc7
sistemas.roger <sistemas.roger@d0083e2e-fec8-11dd-802b-177316e4ac06>
2009-05-11 04:11:04 +0000

    Testes

--- vendors/TestSuite/app/vendors/simpletest/test/test_with_parse_error.php
+++ vendors/TestSuite/app/vendors/simpletest/test/test_with_parse_error.php
@@ -2,7 +2,7 @@
     // $Id: test_with_parse_error.php 4 2006-04-25 02:08:26Z phpnut $
     
     class TestCaseWithParseError extends UnitTestCase {
-        //wibble;
+        wibble
     }
     
 ?>
